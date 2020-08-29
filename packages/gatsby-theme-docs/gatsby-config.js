383644ca0af13101363d3c9af1bddef5275b5ff6
maureenlholland <maureen.holland11@gmail.com>
2020-08-29 17:01:26 -0400

    load mdx into graphql

---
+++ packages/gatsby-theme-docs/gatsby-config.js
@@ -0,0 +1,29 @@
+const withDefaults = require('./utils/default-options')
+
+// in theme, config is function with options arg
+module.exports = options => {
+    const { contentPath, useExternalMDX } = withDefaults(options);
+
+    // now you can return reg config object
+    return {
+        plugins: [
+            {
+                // always resolve
+                resolve: 'gatsby-source-filesystem',
+                options: {
+                    // we want to be able to identify which files were loaded by this theme
+                    name: 'gatsby-theme-docs',
+                    path: contentPath
+                }
+            },
+            !useExternalMDX && {
+                resolve: 'gatsby-plugin-mdx',
+                options: {
+                    defaultLayouts: {
+                        default: require.resolve('./src/components/layout.js'),
+                    }
+                }
+            }
+        ].filter(Boolean) // need to cut out the potential false value
+    }
+}
