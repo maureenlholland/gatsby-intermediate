d96aab45921312c9b0a6bde572e37e01b7291d83
maureenlholland <maureen.holland11@gmail.com>
2020-08-29 22:03:42 -0400

    create node instance, custom resolver

--- packages/gatsby-theme-docs/gatsby-node.js
+++ packages/gatsby-theme-docs/gatsby-node.js
@@ -15,4 +15,73 @@ exports.onPreBootstrap = ({ store }, options) => {
         // create dir
         mkdirp.sync(dir);
     }
+};
+
+exports.createSchemaCustomization = ({ actions }) => {
+    // allows us to create custom datatype that does exactly what we want it to do
+    // Node is Gatsby's core type. Any data that exists will be given Node type.
+    actions.createTypes(`
+        type DocsPage implements Node @dontInfer {
+            id: ID!
+            title: String!
+            path: String!
+            updated: Date! @dateformat
+            body: String!
+        }
+    `)
+};
+
+// not all of the node types are fully resolved by the time we reach onCreateNode, so we need a custom Resolver
+exports.onCreateNode = ({ node, actions, getNode, createNodeId }, options) => {
+    const { basePath } = withDefaults(options);
+    const parent = getNode(node.parent);
+    
+    // if not MDX files loaded by this theme, early return (sourceInstanceName set in gatsby-config)
+    if (
+        node.internal.type !== 'Mdx' ||
+        (parent.sourceInstanceName !== 'gatsby-theme-docs')
+    ) {
+        return
+    }
+
+    // treat index.mdx like index.html, plain basepath (ie docs) instead of docs/index
+    const pageName = parent.name !== 'index' ? parent.name : '';
+
+    // create the DocsPage node we want to query with GraphQL
+    // every gatsby node needs the internal key
+    actions.createNode({
+        id: createNodeId(`DocsPage-${node.id}`),
+        title: node.frontmatter.title || parent.name,
+        updated: parent.modifiedTime,
+        path: path.join('/', basePath, parent.relativeDirectory, pageName),
+        parent: node.id,
+        internal: {
+            type: 'DocsPage',
+            contentDigest: node.internal.contentDigest,
+        }
+    });
+};
+
+// we need to resolve the mdx body
+exports.createResolvers = ({ createResolvers }) => {
+    createResolvers({
+        DocsPage: {
+            body: {
+                type: 'String!',
+                resolve: (source, args, context, info) => {
+                    // load the resolver for the MDX type for body field
+                    const type = info.schema.getType('Mdx');
+                    const mdxFields = type.getFields(); // this is where we get body
+                    // passthrough resolver, we are using the existing DocsPage resolver
+                    const resolver = mdxFields.body.resolve;
+
+                    const mdxNode = context.nodeModel.getNodeById({ id: source.parent });
+
+                    return resolver(mdxNode, args, context, {
+                        fieldName: 'body',
+                    });
+                } 
+            }
+        }
+    });
 }
