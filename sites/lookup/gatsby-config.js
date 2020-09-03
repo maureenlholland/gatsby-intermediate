862aa2fed56462d90b688bfda3a667c71ef829ae
maureenlholland <maureen.holland11@gmail.com>
2020-09-03 15:58:33 -0400

    note on client api needing token

--- sites/lookup/gatsby-config.js
+++ sites/lookup/gatsby-config.js
@@ -1,4 +1,5 @@
 // gatsby helper & simple api allow this to work, usually you need to manually configure the graphql client
+// to use an api with a secret, you want to use serverless functions (to say this function can only be called from my domain), client side is not secure
 module.exports  = {
     plugins: [
         {
